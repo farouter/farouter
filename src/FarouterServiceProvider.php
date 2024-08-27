ad839b768260751e342779776acfa83113acf012
Jaroslaw Goszowski <goszowski@gmail.com>
2024-08-27 17:19:40 +0300

    base config

--- src/FarouterServiceProvider.php
+++ src/FarouterServiceProvider.php
@@ -8,11 +8,15 @@ class FarouterServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'farouter');
     }
 
     public function boot()
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('farouter.php'),
+            ], 'config');
+        }
     }
 }
