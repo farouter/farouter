2f8b07951634011600f516d2d54f43fe5d8bd29c
Jaroslaw Goszowski <goszowski@gmail.com>
2024-12-16 12:51:11 +0200

    init

---
+++ src/FarouterServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Farouter;
+
+use Illuminate\Support\ServiceProvider;
+
+class FarouterServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
